1147f6328bf1f2a2f705e300607d73e989a7b097
Alexander Lucano <sailex1030@gmail.com>
2021-03-17 13:23:04 -0500

    Add ability to make posts from the administrator interface/Agregamos la funcionalidad de hacer post desde el panel de administrador.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+# estamos importando la clase Posts para poder mostrarlo en el admin
+from .models import Post
+# Register your models here.Esto nos muestra el ventana,pestaña post para utilizarla
+# en el admin
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+# create a string representation
+# aqui mostramos en string el titulo del post y lo que queramos
+    def __str__(self):
+        return self.title
+        # return self.title + ' por ' + str(self.author)
