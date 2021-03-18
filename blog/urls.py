4b7bbaabe4f73442346d55ee8de339f51ae291af
Alexander Lucano <sailex1030@gmail.com>
2021-03-17 19:43:19 -0500

    Add detail view for each blog post./Agregando el redirecionamiento a los post haciendo click en ellos.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 # esto te llevara a los viewst post donde tenemos definido unas views y eso mostrara
 # recordar qwue tambien tenemos que cambiar las urls de mysite
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
