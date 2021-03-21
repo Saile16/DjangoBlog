88963b02bd8ffab3ec5c361737629e83ce0a3635
Alexander Lucano <sailex1030@gmail.com>
2021-03-21 11:23:11 -0500

    Add ability to delete blog posts./Agregamos funcionalidad de eliminar posts.

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     # 127.0.0.1:8000/post/new-->local
     path('post/new/', views.post_new, name="post_new"),
     path('post/<int:pk>/edit/', views.post_edit, name="post_edit"),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     #
@@ -18,6 +19,6 @@ urlpatterns = [
          name='add_comment_to_post'),
     path('comment/<int:pk>/remove/', views.comment_remove, name="comment_remove"),
     path('comment/<int:pk>/approve/',
-         views.comment_approve, name='comment_approve')
+         views.comment_approve, name='comment_approve'),
 
 ]
